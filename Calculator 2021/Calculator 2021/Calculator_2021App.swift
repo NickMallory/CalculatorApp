a15e9985280bd72188b16b1caa27891a2240371c
NickMallory <88634472+NickMallory@users.noreply.github.com>
2022-03-26 23:16:44 +0200

    Add files via upload

---
+++ Calculator 2021/Calculator 2021/Calculator_2021App.swift
@@ -0,0 +1,18 @@
+//
+//  Calculator_2021App.swift
+//  Calculator 2021
+//
+//  Created by IACD-Air-9 on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        
+        }
+    }
+}
